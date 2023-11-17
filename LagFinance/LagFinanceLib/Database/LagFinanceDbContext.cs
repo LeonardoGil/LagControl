7d994b39b831d4c442d1f27961e62f2bcc111ae0
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2023-11-17 15:23:19 -0300

    Alterado database em connectionString

--- LagFinance/LagFinanceLib/Database/LagFinanceDbContext.cs
+++ LagFinance/LagFinanceLib/Database/LagFinanceDbContext.cs
@@ -6,7 +6,7 @@ namespace LagFinanceLib.Database
     public class LagFinanceDbContext : DbContext
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-            => optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=LagFinance;Persist Security Info=True;User ID=sa;Password=P@ssw0rd!;Connect Timeout=900;TrustServerCertificate=true");
+            => optionsBuilder.UseSqlServer(@"Data Source=localhost;Initial Catalog=LagControl;Persist Security Info=True;User ID=sa;Password=P@ssw0rd!;Connect Timeout=900;TrustServerCertificate=true");
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
