4446d4f3ff5d1f580a6e9d65e246ce42cf43969e
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2023-11-22 22:02:25 -0300

    Configurando Dependency Injection Context

--- CLI/LagControlCLI/Configs/DependencyInjection.cs
+++ CLI/LagControlCLI/Configs/DependencyInjection.cs
@@ -2,8 +2,10 @@
 using LagControlCLI.Commands.Interfaces;
 using LagControlCLI.Services.Finance;
 using LagControlCLI.Services.Interfaces;
+using LagFinanceLib.Database;
 using LagFinanceLib.Interfaces;
 using LagFinanceLib.Repository;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -13,6 +15,10 @@ namespace LagControlCLI.Configs
     {
         public static void Inject(HostApplicationBuilder builder)
         {
+            #region Context
+            builder.Services.AddDbContext<LagFinanceDbContext>(opt => opt.UseSqlServer(@"Data Source=localhost;Initial Catalog=LagControl;Persist Security Info=True;User ID=sa;Password=P@ssw0rd!;Connect Timeout=900;TrustServerCertificate=true"));
+            #endregion
+
             #region CLI
             builder.Services.AddSingleton<IFinanceCommand, FinanceCommand>();
             builder.Services.AddSingleton<IFinanceAddServices, FinanceAddServices>();
