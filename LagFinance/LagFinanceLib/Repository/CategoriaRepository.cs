d1d6506e464e1be81da0c3dcb72dbe92a26c7d00
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2023-12-03 09:58:54 -0300

    Adicionado CategoriaRepository

---
+++ LagFinance/LagFinanceLib/Repository/CategoriaRepository.cs
@@ -0,0 +1,14 @@
+﻿using LagFinanceLib.Database;
+using LagFinanceLib.Domain;
+using LagFinanceLib.Interfaces;
+
+namespace LagFinanceLib.Repository
+{
+    public class CategoriaRepository : BaseRepository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(LagFinanceDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
