6f693479130bb06161d924c19e64106b7cd32b09
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2023-12-05 22:17:36 -0300

    Alinhamento dos controles

--- Forms/LagControlForms/Forms/MovimentacaoForms/Controls/AdicionarMovimentacaoControl.Designer.cs
+++ Forms/LagControlForms/Forms/MovimentacaoForms/Controls/AdicionarMovimentacaoControl.Designer.cs
@@ -42,12 +42,13 @@
             labelData = new Label();
             comboBoxContaTransferencia = new ComboBox();
             buttonGravarRepetir = new Button();
+            labelContaTransferencia = new Label();
             SuspendLayout();
             // 
             // labelDescricao
             // 
             labelDescricao.AutoSize = true;
-            labelDescricao.Location = new Point(12, 15);
+            labelDescricao.Location = new Point(12, 12);
             labelDescricao.Name = "labelDescricao";
             labelDescricao.Size = new Size(58, 15);
             labelDescricao.TabIndex = 0;
@@ -55,14 +56,14 @@
             // 
             // textBoxDescricao
             // 
-            textBoxDescricao.Location = new Point(12, 33);
+            textBoxDescricao.Location = new Point(12, 30);
             textBoxDescricao.Name = "textBoxDescricao";
             textBoxDescricao.Size = new Size(218, 23);
             textBoxDescricao.TabIndex = 1;
             // 
             // textBoxValor
             // 
-            textBoxValor.Location = new Point(12, 132);
+            textBoxValor.Location = new Point(12, 136);
             textBoxValor.Name = "textBoxValor";
             textBoxValor.Size = new Size(78, 23);
             textBoxValor.TabIndex = 7;
@@ -71,7 +72,7 @@
             // labelValor
             // 
             labelValor.AutoSize = true;
-            labelValor.Location = new Point(12, 114);
+            labelValor.Location = new Point(12, 118);
             labelValor.Name = "labelValor";
             labelValor.Size = new Size(33, 15);
             labelValor.TabIndex = 6;
@@ -81,7 +82,7 @@
             // 
             checkedListBoxTipoMovimentacao.CheckOnClick = true;
             checkedListBoxTipoMovimentacao.FormattingEnabled = true;
-            checkedListBoxTipoMovimentacao.Location = new Point(12, 168);
+            checkedListBoxTipoMovimentacao.Location = new Point(12, 171);
             checkedListBoxTipoMovimentacao.Name = "checkedListBoxTipoMovimentacao";
             checkedListBoxTipoMovimentacao.Size = new Size(113, 58);
             checkedListBoxTipoMovimentacao.TabIndex = 10;
@@ -90,7 +91,7 @@
             // comboBoxCategoria
             // 
             comboBoxCategoria.FormattingEnabled = true;
-            comboBoxCategoria.Location = new Point(98, 132);
+            comboBoxCategoria.Location = new Point(98, 136);
             comboBoxCategoria.Name = "comboBoxCategoria";
             comboBoxCategoria.Size = new Size(132, 23);
             comboBoxCategoria.TabIndex = 9;
@@ -98,7 +99,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(98, 114);
+            label1.Location = new Point(98, 118);
             label1.Name = "label1";
             label1.Size = new Size(58, 15);
             label1.TabIndex = 8;
@@ -107,7 +108,7 @@
             // labelConta
             // 
             labelConta.AutoSize = true;
-            labelConta.Location = new Point(98, 64);
+            labelConta.Location = new Point(98, 65);
             labelConta.Name = "labelConta";
             labelConta.Size = new Size(39, 15);
             labelConta.TabIndex = 4;
@@ -116,7 +117,7 @@
             // comboBoxConta
             // 
             comboBoxConta.FormattingEnabled = true;
-            comboBoxConta.Location = new Point(98, 82);
+            comboBoxConta.Location = new Point(98, 83);
             comboBoxConta.Name = "comboBoxConta";
             comboBoxConta.Size = new Size(132, 23);
             comboBoxConta.TabIndex = 5;
@@ -126,14 +127,14 @@
             buttonGravar.Location = new Point(12, 272);
             buttonGravar.Name = "buttonGravar";
             buttonGravar.Size = new Size(102, 35);
-            buttonGravar.TabIndex = 12;
+            buttonGravar.TabIndex = 13;
             buttonGravar.Text = "Gravar";
             buttonGravar.UseVisualStyleBackColor = true;
             buttonGravar.Click += Save_ClickEvent;
             // 
             // maskedTextBoxData
             // 
-            maskedTextBoxData.Location = new Point(12, 82);
+            maskedTextBoxData.Location = new Point(12, 83);
             maskedTextBoxData.Mask = "00/00/0000";
             maskedTextBoxData.Name = "maskedTextBoxData";
             maskedTextBoxData.Size = new Size(78, 23);
@@ -144,7 +145,7 @@
             // labelData
             // 
             labelData.AutoSize = true;
-            labelData.Location = new Point(12, 64);
+            labelData.Location = new Point(12, 65);
             labelData.Name = "labelData";
             labelData.Size = new Size(31, 15);
             labelData.TabIndex = 2;
@@ -153,10 +154,10 @@
             // comboBoxContaTransferencia
             // 
             comboBoxContaTransferencia.FormattingEnabled = true;
-            comboBoxContaTransferencia.Location = new Point(12, 239);
+            comboBoxContaTransferencia.Location = new Point(131, 206);
             comboBoxContaTransferencia.Name = "comboBoxContaTransferencia";
-            comboBoxContaTransferencia.Size = new Size(113, 23);
-            comboBoxContaTransferencia.TabIndex = 11;
+            comboBoxContaTransferencia.Size = new Size(99, 23);
+            comboBoxContaTransferencia.TabIndex = 12;
             comboBoxContaTransferencia.Visible = false;
             // 
             // buttonGravarRepetir
@@ -164,15 +165,26 @@
             buttonGravarRepetir.Location = new Point(126, 272);
             buttonGravarRepetir.Name = "buttonGravarRepetir";
             buttonGravarRepetir.Size = new Size(102, 35);
-            buttonGravarRepetir.TabIndex = 13;
+            buttonGravarRepetir.TabIndex = 14;
             buttonGravarRepetir.Text = "Gravar e Repetir";
             buttonGravarRepetir.UseVisualStyleBackColor = true;
             buttonGravarRepetir.Click += SaveAndRepeat_ClickEvent;
+            // 
+            // labelContaTransferencia
+            // 
+            labelContaTransferencia.AutoSize = true;
+            labelContaTransferencia.Location = new Point(131, 173);
+            labelContaTransferencia.Name = "labelContaTransferencia";
+            labelContaTransferencia.Size = new Size(76, 30);
+            labelContaTransferencia.TabIndex = 11;
+            labelContaTransferencia.Text = "Conta \r\nTransferencia";
+            labelContaTransferencia.Visible = false;
             // 
             // AdicionarMovimentacaoControl
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(labelContaTransferencia);
             Controls.Add(buttonGravarRepetir);
             Controls.Add(comboBoxContaTransferencia);
             Controls.Add(labelData);
@@ -210,5 +222,6 @@
         private Label labelData;
         private ComboBox comboBoxContaTransferencia;
         private Button buttonGravarRepetir;
+        private Label labelContaTransferencia;
     }
 }
