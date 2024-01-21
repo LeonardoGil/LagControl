7d900e61d530b9c36547b335995c7dd990ecc717
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-01-20 22:14:49 -0300

    Tema basico

--- Forms/LagControlForms/Forms/MainForm.Designer.cs
+++ Forms/LagControlForms/Forms/MainForm.Designer.cs
@@ -28,21 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel1 = new Panel();
+            pictureBoxLogo = new PictureBox();
+            panelView = new Panel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).BeginInit();
             SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(84, 8, 28);
+            panel1.Controls.Add(pictureBoxLogo);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(984, 130);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBoxLogo
+            // 
+            pictureBoxLogo.BackgroundImage = Properties.Resources.Lag_removebg_preview;
+            pictureBoxLogo.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBoxLogo.Dock = DockStyle.Left;
+            pictureBoxLogo.Location = new Point(0, 0);
+            pictureBoxLogo.Name = "pictureBoxLogo";
+            pictureBoxLogo.Size = new Size(230, 130);
+            pictureBoxLogo.TabIndex = 0;
+            pictureBoxLogo.TabStop = false;
+            // 
+            // panelView
+            // 
+            panelView.Dock = DockStyle.Fill;
+            panelView.Location = new Point(0, 130);
+            panelView.Margin = new Padding(0);
+            panelView.Name = "panelView";
+            panelView.Padding = new Padding(5);
+            panelView.Size = new Size(984, 631);
+            panelView.TabIndex = 1;
             // 
             // MainForm
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 19F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = Color.FromArgb(14, 14, 14);
-            ClientSize = new Size(800, 600);
-            FormBorderStyle = FormBorderStyle.None;
+            BackColor = Color.FromArgb(11, 1, 11);
+            ClientSize = new Size(984, 761);
+            Controls.Add(panelView);
+            Controls.Add(panel1);
+            Font = new Font("SauceCodePro NF Medium", 11.25F, FontStyle.Bold, GraphicsUnit.Point);
+            ForeColor = Color.DarkGray;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Margin = new Padding(4, 3, 4, 3);
             Name = "MainForm";
+            Opacity = 0.98D;
+            ShowIcon = false;
+            ShowInTaskbar = false;
             StartPosition = FormStartPosition.CenterScreen;
-            Text = "MainForm";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBoxLogo).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBoxLogo;
+        private Panel panelView;
     }
 }
--- Forms/LagControlForms/Forms/MainForm.cs
+++ Forms/LagControlForms/Forms/MainForm.cs
@@ -2,6 +2,15 @@
 {
     public partial class MainForm : Form
     {
+        /*  Pallete:
+         * 
+         *  #0b010b - Black
+         *  #1e2037 - Gray
+         *  #54081c - Red
+         *  #7d715d - Broown
+        */
+
+
         public MainForm()
         {
             InitializeComponent();
