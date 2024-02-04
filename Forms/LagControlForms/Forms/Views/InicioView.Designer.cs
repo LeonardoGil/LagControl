a6ec268f52ff7c558966a6240c8f63a4344f49b4
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-02-04 07:47:54 -0300

    Create Inicio View control

---
+++ Forms/LagControlForms/Forms/Views/InicioView.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace LagControlForms.Forms.Views
+{
+    partial class InicioView
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panelHeader = new Panel();
+            labelServices = new Label();
+            flowPanelServices = new FlowLayoutPanel();
+            panelHeader.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panelHeader
+            // 
+            panelHeader.Controls.Add(labelServices);
+            panelHeader.Dock = DockStyle.Top;
+            panelHeader.Location = new Point(0, 0);
+            panelHeader.Margin = new Padding(0);
+            panelHeader.MaximumSize = new Size(0, 60);
+            panelHeader.Name = "panelHeader";
+            panelHeader.Size = new Size(784, 60);
+            panelHeader.TabIndex = 0;
+            // 
+            // labelServices
+            // 
+            labelServices.AutoSize = true;
+            labelServices.Font = new Font("SauceCodePro NF SemiBold", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            labelServices.Location = new Point(25, 25);
+            labelServices.Name = "labelServices";
+            labelServices.Size = new Size(98, 24);
+            labelServices.TabIndex = 0;
+            labelServices.Text = "Serviços";
+            // 
+            // flowPanelServices
+            // 
+            flowPanelServices.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            flowPanelServices.Location = new Point(0, 60);
+            flowPanelServices.MaximumSize = new Size(0, 200);
+            flowPanelServices.Name = "flowPanelServices";
+            flowPanelServices.Padding = new Padding(25, 10, 10, 25);
+            flowPanelServices.Size = new Size(784, 120);
+            flowPanelServices.TabIndex = 1;
+            flowPanelServices.Resize += FlowPanelServices_ResizeEvent;
+            // 
+            // InicioView
+            // 
+            AutoScaleDimensions = new SizeF(10F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(flowPanelServices);
+            Controls.Add(panelHeader);
+            Name = "InicioView";
+            Load += View_LoadEvent;
+            panelHeader.ResumeLayout(false);
+            panelHeader.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelHeader;
+        private Label labelServices;
+        private FlowLayoutPanel flowPanelServices;
+    }
+}
---
+++ Forms/LagControlForms/Forms/Views/InicioView.cs
@@ -0,0 +1,36 @@
+﻿using LagControlForms.Forms.Controls.Buttons;
+
+namespace LagControlForms.Forms.Views
+{
+    public partial class InicioView : BaseView
+    {
+        public InicioView()
+        {
+            InitializeComponent();
+        }
+
+        private void View_LoadEvent(object sender, EventArgs e)
+        {
+            var finance = new DisplayButton
+            {
+                Resource = "dinheiro",
+                Text = "Finanças"
+            };
+
+            flowPanelServices.Controls.Add(finance);
+        }
+
+        private void FlowPanelServices_ResizeEvent(object sender, EventArgs e)
+        {
+            if (sender is Control controlParent)
+            {
+                var size = controlParent.Height - 120 + 100;
+
+                foreach (var control in controlParent.Controls.OfType<Control>())
+                {
+                    control.Size = new Size(size, size);
+                };
+            }
+        }
+    }
+}
