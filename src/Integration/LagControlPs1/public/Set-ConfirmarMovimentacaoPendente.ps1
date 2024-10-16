b374ce54a5fe521da0f73bf11367b44e1bfcaa0d
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2024-10-16 06:58:06 -0300

    Desenvolvimento do comando Set-ConfirmarMovimentacaoPendente

--- src/Integration/LagControlPs1/public/Set-ConfirmarMovimentacaoPendente.ps1
+++ src/Integration/LagControlPs1/public/Set-ConfirmarMovimentacaoPendente.ps1
@@ -2,34 +2,64 @@ function Set-ConfirmarMovimentacaoPendente {
 
     [CmdletBinding()]
     param (
+        [Parameter()]
+        [string]
+        $Descricao,
 
+        [Parameter()]
+        [string]
+        $Observacao,
+
+        [Parameter()]
+        [decimal]
+        $Valor,
+
+        [Parameter()]
+        [datetime]
+        $Data
     )
 
     $movimentacoes = Invoke-RestMethod -Method 'Get' -Uri 'https://localhost:7081/Movimentacao/Listar?ApenasPendentes=true'
 
-    $movimentacao = Show-ConfirmarMovimentacaoPendenteOptions $movimentacoes
+    Write-Output $movimentacoes.GetType()
 
-    $body = [PSCustomObject]@{
-        Id = $movimentacao.Id
-    } 
+    $movimentacao = Show-ConfirmarMovimentacaoPendenteOptions $movimentacoes 
+
+    $body = New-ConfirmarMovimentacaoPendenteObject $movimentacao
 
     Invoke-RestMethod -Method 'Post' -Uri 'https://localhost:7081/Movimentacao/Confirmar-Pendente' -Body ($body | ConvertTo-Json) -ContentType "application/json; charset=utf-8" -Headers @{ "Accept" = "application/json" }
 }
+
+function New-ConfirmarMovimentacaoPendenteObject {
+
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]
+        [PSCustomObject]
+        $movimentacao
+    )
+
+    $body = [PSCustomObject]@{
+        Id = $movimentacao.Id
+    }
+    
+    return $body
+}
+
 
 function Show-ConfirmarMovimentacaoPendenteOptions {
 
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
-        [PSCustomObject]
+        [PSCustomObject[]]
         $movimentacoes
     )
 
     $choices = $movimentacoes | ForEach-Object { 
-
-        $value = "&$([Array]::IndexOf($movimentacoes, $_)) $(Get-Date $_.Data -Format 'dd/MM') $($_.ValorTotal) $($_.descricao)"
-
-        return [ChoiceDescription]::new($value , $_.descricao) 
+        $message = "&$([Array]::IndexOf($movimentacoes, $_)) $($_.descricao)"
+        $helpMessage = "$(Get-Date $_.Data -Format 'dd/MM/yyyy') R$ $($_.Valor) $($_.descricao)"
+        return [ChoiceDescription]::new($message , $helpMessage) 
     }
 
     $movimentacao = $host.UI.PromptForChoice("Escolha uma movimentação", "", $choices, 0)
