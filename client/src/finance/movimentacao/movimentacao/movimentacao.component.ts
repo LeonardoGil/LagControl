bd3e8562bef29bd9b4540ce3e10ed4349211d4aa
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2025-02-21 07:40:54 -0300

    feat: Desenvolvimento da tela de Movimentação

--- client/src/finance/movimentacao/movimentacao/movimentacao.component.ts
+++ client/src/finance/movimentacao/movimentacao/movimentacao.component.ts
@@ -1,11 +1,35 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import {MatTableModule} from '@angular/material/table';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-movimentacao',
-  imports: [],
+  imports: [MatTableModule,
+    HttpClientModule
+  ],
+  standalone: true,
   templateUrl: './movimentacao.component.html',
   styleUrl: './movimentacao.component.css'
 })
 export class MovimentacaoComponent {
+  protected Movimentacoes: MovimentacaoGrid[] = []
+  protected Colunas: string[] = ['descricao']
 
+  constructor(private httpClient: HttpClient) {
+    this.ObterDadosGrid().subscribe(
+      (result) => {
+        this.Movimentacoes = result
+        console.log(this.Movimentacoes)
+      }
+    )
+  }
+
+  ObterDadosGrid() : Observable<MovimentacaoGrid[]> {
+    return this.httpClient.get<MovimentacaoGrid[]>('https://localhost:7081/Movimentacao/Listar?ContaIds=9ab68e5a-a829-40b9-9d32-b9746d3134f5');
+  }
+}
+
+export interface MovimentacaoGrid {
+  descricao: string
 }
