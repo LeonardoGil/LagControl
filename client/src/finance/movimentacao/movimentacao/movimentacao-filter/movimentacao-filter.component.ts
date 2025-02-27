2933bf7d8fcaee8c37a4702f11830fc69134e927
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2025-02-27 06:07:08 -0300

    feat: Abstração de Component (movimentacao-filter)

---
+++ client/src/finance/movimentacao/movimentacao/movimentacao-filter/movimentacao-filter.component.ts
@@ -0,0 +1,116 @@
+import { ChangeDetectionStrategy, Component, OnInit, OnDestroy, inject } from '@angular/core';
+import { HttpParams } from '@angular/common/http';
+import { Subscription } from 'rxjs';
+import { format } from 'date-fns';
+
+import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
+
+import { commonProviders } from './../../../../share/providers/common.provider';
+
+import { Categoria } from '../../../categoria/models/categoria.model';
+import { TipoMovimentacaoEnum } from '../../models/tipoMovimentacao.model';
+
+import { CategoriaService } from './../../../categoria/services/categoria.service';
+import { MovimentacaoService } from '../../services/movimentacao.service';
+
+@Component({
+  selector: 'app-movimentacao-filter',
+  standalone: true,
+  imports: [
+    commonProviders
+  ],
+  providers: [
+    provideNativeDateAdapter(),
+    {
+      provide: MAT_DATE_LOCALE, 
+      useValue: 'pt-br'
+    }
+  ],
+  templateUrl: './movimentacao-filter.component.html',
+  styleUrl: './movimentacao-filter.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class MovimentacaoFilterComponent implements OnInit, OnDestroy {
+
+  private movimentacaoService: MovimentacaoService = inject(MovimentacaoService)
+  private categoriaService: CategoriaService = inject(CategoriaService)
+  private subscription!: Subscription
+  
+  categorias: Categoria[] = []
+  filterModel = new MovimentacaoFilter()
+  
+  ngOnInit(): void {
+    this.categoriaService.Listar(new HttpParams()).subscribe((categorias: Categoria[]) => this.categorias = categorias)
+    
+    this.filterModel.DataInicial = new Date(2025, 2, 1)
+    this.filterModel.DataFinal = new Date(2025, 2, 28)
+
+    this.filterMovimentacoes(undefined)
+  }
+  
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  } 
+
+  protected TipoMovimentacaoOptions = [
+    { 
+      label: '',
+      value: null
+    },
+    { 
+      label: 'Receita',
+      value: 0 as TipoMovimentacaoEnum
+    },
+    {
+      label: 'Despesa',
+      value: 1  as TipoMovimentacaoEnum
+    },
+    {
+      label: 'Transferencia',
+      value: 2 as TipoMovimentacaoEnum
+    }
+  ]
+ 
+  filterMovimentacoes(teste: any) {
+    let params = new HttpParams()
+
+    params = params.set('ContaIds', '9ab68e5a-a829-40b9-9d32-b9746d3134f5')
+    params = params.set('ApenasPendentes', this.filterModel.Pendente.toString())
+
+    if (this.filterModel.Descricao !== undefined && this.filterModel.Descricao != '') {
+        params = params.set('Descricao', this.filterModel.Descricao)
+    }
+
+    if (this.filterModel.CategoriaId !== undefined && this.filterModel.CategoriaId != '') {
+      params = params.set('CategoriaIds', this.filterModel.CategoriaId)
+    } 
+
+    if (this.filterModel.Tipo !== undefined) {
+      params = params.set('Tipo', this.filterModel.Tipo)
+    }
+
+    if (this.filterModel.DataInicial !== undefined && this.filterModel.DataInicial != null 
+      && this.filterModel.DataFinal !== undefined && this.filterModel.DataFinal != null) {
+
+        this.filterModel.DataFinal.setHours(0, 0, 0)
+        params = params.set('DataInicial', format(this.filterModel.DataInicial, 'yyyy-MM-dd\'T\'HH:mm:ss'))
+      
+        this.filterModel.DataFinal.setHours(23, 59, 59)  
+        params = params.set('DataFinal', format(this.filterModel.DataFinal, 'yyyy-MM-dd\'T\'HH:mm:ss'))
+    }
+    
+    this.subscription = this.movimentacaoService.Listar(params).subscribe()
+  }
+}
+
+export class MovimentacaoFilter {
+  Descricao!: string
+  Tipo!: TipoMovimentacaoEnum
+  Pendente: boolean = false
+
+  CategoriaId!: string
+  ContaId!: string
+
+  DataInicial!: Date
+  DataFinal!: Date
+}
--- client/src/finance/movimentacao/movimentacao/movimentacao.component.ts
+++ client/src/finance/movimentacao/movimentacao/movimentacao.component.ts
@@ -1,101 +1,22 @@
-import { inject } from '@angular/core';
-import { ChangeDetectionStrategy, Component, ViewChild, AfterViewInit } from '@angular/core';
-import { HttpParams } from '@angular/common/http';
-import { format } from 'date-fns';
-
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { commonProviders } from './../../../share/providers/common.provider';
 
-import { MAT_DATE_LOCALE, provideNativeDateAdapter } from '@angular/material/core';
-
-import { CategoriaService } from './../../categoria/services/categoria.service';
-import { MovimentacaoService } from './../services/movimentacao.service';
-
-import { Categoria } from '../../categoria/models/categoria.model';
-import { TipoMovimentacaoEnum } from '../models/tipoMovimentacao.model';
 import { MovimentacaoGridComponent } from "./movimentacao-grid/movimentacao-grid.component";
+import { MovimentacaoFilterComponent } from "./movimentacao-filter/movimentacao-filter.component";
 
 @Component({
   imports: [
     ...commonProviders,
     
-    MovimentacaoGridComponent
-],
-  providers: [
-    provideNativeDateAdapter(),
-    {
-      provide: MAT_DATE_LOCALE, 
-      useValue: 'pt-br'
-    },
-  ],
+    MovimentacaoGridComponent,
+    MovimentacaoFilterComponent
+],  
   standalone: true,
   templateUrl: './movimentacao.component.html',
   styleUrl: './movimentacao.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MovimentacaoComponent implements AfterViewInit {
+export class MovimentacaoComponent {
   
-  private movimentacaoService: MovimentacaoService = inject(MovimentacaoService)
-  private CategoriaService: CategoriaService = inject(CategoriaService)
-  
-  ngAfterViewInit(): void {
-    this.CategoriaService.Listar(new HttpParams()).subscribe((categorias: Categoria[]) => this.categorias = categorias)
-    
-    this.filterModel.DataInicial = new Date(2025, 2, 1)
-    this.filterModel.DataFinal = new Date(2025, 2, 28)
-
-    this.filterMovimentacoes(undefined)
-  }
-  
-  categorias: Categoria[] = []
-  filterModel = new MovimentacaoFilter()
-
-  protected TipoMovimentacaoOptions = Object.keys(TipoMovimentacaoEnum)
-                                            .filter(key => isNaN(Number(key)))
-                                            .map(key => ({
-                                                label: key,
-                                                value: TipoMovimentacaoEnum[key as keyof typeof TipoMovimentacaoEnum]
-                                            }))
- 
-  filterMovimentacoes(teste: any) {
-    let params = new HttpParams()
-
-    params = params.set('ContaIds', '9ab68e5a-a829-40b9-9d32-b9746d3134f5')
-    params = params.set('ApenasPendentes', this.filterModel.Pendente.toString())
-
-    if (this.filterModel.Descricao !== undefined && this.filterModel.Descricao != '') {
-        params = params.set('Descricao', this.filterModel.Descricao)
-    }
-
-    if (this.filterModel.CategoriaId !== undefined && this.filterModel.CategoriaId != '') {
-      params = params.set('CategoriaIds', this.filterModel.CategoriaId)
-    } 
-
-    if (this.filterModel.Tipo !== undefined) {
-      params = params.set('Tipo', this.filterModel.Tipo)
-    }
-
-    if (this.filterModel.DataInicial !== undefined && this.filterModel.DataInicial != null 
-      && this.filterModel.DataFinal !== undefined && this.filterModel.DataFinal != null) {
-
-        this.filterModel.DataFinal.setHours(0, 0, 0)
-        params = params.set('DataInicial', format(this.filterModel.DataInicial, 'yyyy-MM-dd\'T\'HH:mm:ss'))
-      
-        this.filterModel.DataFinal.setHours(23, 59, 59)  
-        params = params.set('DataFinal', format(this.filterModel.DataFinal, 'yyyy-MM-dd\'T\'HH:mm:ss'))
-    }
-    
-    this.movimentacaoService.Listar(params).subscribe()
-  }
 }
 
-export class MovimentacaoFilter {
-  Descricao!: string
-  Tipo!: TipoMovimentacaoEnum
-  Pendente: boolean = false
-
-  CategoriaId!: string
-  ContaId!: string
-
-  DataInicial!: Date
-  DataFinal!: Date
-}
