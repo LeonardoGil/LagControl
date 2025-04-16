afe52e7d4c4fca126e103f8ecc664ce74355b711
Leonardo Albuquerque Gil <leonardoalbuquerquegil@hotmail.com>
2025-04-16 06:16:30 -0300

    feat: Desenvolvimento tela de listar Categorias (estrutura)

--- client/src/finance/categoria/categoria-listar/categoria-listar.component.ts
+++ client/src/finance/categoria/categoria-listar/categoria-listar.component.ts
@@ -1,23 +1,59 @@
+import { commonProviders } from './../../../share/providers/common.provider';
 import { AppService } from './../../../app/app.service';
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild, inject } from '@angular/core';
+import { MatAccordion } from '@angular/material/expansion';
+import { Categoria } from '../models/categoria.model';
+import { CategoriaService } from '../services/categoria.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-categoria-listar',
   standalone: true,
   imports: [
-    CommonModule,
+    ...commonProviders,
   ],
   templateUrl: './categoria-listar.component.html',
   styleUrl: './categoria-listar.component.css',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CategoriaListarComponent {
+export class CategoriaListarComponent implements OnInit, OnDestroy {
+  
+  @ViewChild(MatAccordion) accordion!: MatAccordion
 
+  protected categorias: Categoria[] = []
+  protected mostrar: boolean = false
+  
+  private categoriaService: CategoriaService = inject(CategoriaService);
   private appService: AppService = inject(AppService);
+  private destroy$: Subject<void> = new Subject();
 
   constructor() {
     this.appService.definirTitulo('Categoria')
     this.appService.definirNevagacao(['Financeiro', 'Categoria', 'Listar'])
+  }
+
+  ngOnInit(): void {
+    this.categoriaService.listar().pipe(takeUntil(this.destroy$)).subscribe(categorias => {
+      this.categorias = categorias
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.accordion.closeAll();
+  } 
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.unsubscribe();
+  }
+
+  protected clickVisualizar(): void {
+    this.mostrar = !this.mostrar
+
+    if (this.mostrar) {
+      this.accordion.openAll();
+    }
+    else {
+      this.accordion.closeAll();
+    }
   }
  }
